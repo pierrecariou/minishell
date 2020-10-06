fd8cfd1431b412f5b433473f9ec4ccadf09ca902
pierrecariou <pierrecariou@outlook.fr>
2020-10-06 13:11:56 +0200

    parsing almost done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pcariou <pcariou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 10:53:46 by pcariou           #+#    #+#             */
-/*   Updated: 2020/10/01 17:07:25 by pcariou          ###   ########.fr       */
+/*   Updated: 2020/10/06 13:10:48 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* **************************************************************************/
 
@@ -37,6 +37,82 @@ void	check_input(char *buf)
 	ft_putstr_fd("\n", 1);
 }
 
+void	split_input(char *buf, char **words)
+{
+	int i;
+	int c;
+	int l;
+
+	i = 0;
+	c = 0;
+	l = 0;
+	while (buf[i])
+	{
+		if (buf[i] != ' ')
+		{
+		while (buf[i] && buf[i] != ' ')
+		{
+			words[c][l] = buf[i];
+			i++;
+			l++;
+		}
+		words[c][l] = 0;
+		c++;
+		}
+		else
+			i++;
+		l = 0;
+	}
+}
+
+void	malloc_words(char *buf, char **words)
+{
+	int i;
+	int c;
+	int l;
+
+	i = 0;
+	c = 0;
+	l = 0;
+	while (buf[i])
+	{
+		if (buf[i] != ' ')
+		{
+		while (buf[i] && buf[i] != ' ')
+		{
+			i++;
+			l++;
+		}
+		words[c] = malloc(sizeof(char) * l + 1);
+		c++;
+		}
+		else
+			i++;
+		l = 0;
+	}
+}
+
+int		count_words(char *buf)
+{
+	int i;
+	int c;
+
+	i = 0;
+	c = 0;
+	while (buf[i])
+	{
+		if (buf[i] != ' ')
+		{
+			c++;
+		while (buf[i] && buf[i] != ' ')
+			i++;
+		}
+		else
+			i++;
+	}
+	return (c);
+}
+
 void	better_input(char *buf)
 {
 	int i;
@@ -49,11 +125,24 @@ void	better_input(char *buf)
 
 void	read_input(void)
 {
-	char buf[300];
+	//char buf[300];
+	int n;
+	char *buf;
+	char **words;
 
-	read(0, buf, 300);
+	//read(0, buf, 300);
+	(void)n;
+	get_next_line(0, &buf);
 	better_input(buf);
-	check_input(buf);
+	n = count_words(buf);
+	words = malloc(sizeof(char *) * n + 1);
+	words[n + 1] = 0;
+	malloc_words(buf, words);
+	split_input(buf, words);
+	int i = -1;
+	while (words[++i])
+		printf("%s\n", words[i]);
+	//check_input(buf);
 }
 
 void	loop(void)
@@ -64,12 +153,82 @@ void	loop(void)
 		ft_putstr_fd("minishell> ", 1);
 		ft_putstr_fd("\033[0m", 1);
 		read_input();
+		/*
+		if (fork() == 0)
+			execlp("ls", "ls", "-lp", NULL);
+		else
+			wait(NULL);
+		*/
 	}
 }
 
-int		main(void)
+char **split_path(char *path)
 {
-	//signal(SIGINT, handler);
+	char **paths;
+	int i;
+	int c;
+	int k;
+	int l;
+	int m;
+
+	i = -1;
+	c = -1;
+	k = 0;
+	l = 0;
+	m = 0;
+	while (path[++i])
+		k = (path[i] == ':') ? k + 1 : k;
+	paths = malloc(sizeof(char *) * (k + 2));
+	paths[k + 2] = 0;
+	//printf("%d\n", k + 2);
+	m = -1;
+	i = -1;
+	while (++m < k + 1)
+	{
+		while (path[++c] && path[c] != ':')
+			c = c;
+		//printf("%d\n", c - i);
+		paths[m] = malloc(sizeof(char) * (c - i));
+		while (path[++i] && path[i] != ':')
+			paths[m][l++] = path[i];
+		paths[m][l] = 0;
+		l = 0;
+	}
+	return (paths);
+}
+
+
+char	*get_path(char **envp)
+{
+	int i;
+
+	i = -1;
+	while (envp[++i])
+	{
+		if (envp[i][0] == 'P' && envp[i][1] == 'A'
+			&& envp[i][2] == 'T' && envp[i][3] == 'H'
+			&& envp[i][4] == '=')
+			return (&envp[i][5]);
+	}
+	return (0);
+}
+
+int		main(int argc, char **argv, char **envp)
+{
+	char *path;
+	char **paths;
+
+	(void)argc;
+	(void)argv;
+	(void)paths;
+	path = get_path(envp);
+	//path[ft_strlen(path) + 1] = 0;
+	//printf("T>EST --- %s\n", path);
+	//printf("%d\n", ft_strlen(path));
+	paths = split_path(path);
+	int i = -1;
+	while (paths[++i])
+		printf("%s\n", paths[i]);
 	loop();
 	return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: pcariou <pcariou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 10:54:45 by pcariou           #+#    #+#             */
-/*   Updated: 2020/10/01 15:52:44 by pcariou          ###   ########.fr       */
+/*   Updated: 2020/10/06 10:08:15 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <wait.h>
 
 void	ft_putstr_fd(char *s, int fd);
 int		ft_strcmp(char *s1, char *s2);
 int		get_next_line(int fd, char **line);
+int		ft_strlen(char *s1);
 
 #endif
