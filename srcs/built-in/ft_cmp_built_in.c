660f7609c1fa0b0ac64b7f53dd0de37f26ae5612
Gabriel REZETTE <grezette@e2r5p6.42.fr>
2020-10-19 15:11:57 +0200

    corrected an error when calling ft_strncmp in cmp_built_in

--- srcs/built-in/ft_cmp_built_in.c
+++ srcs/built-in/ft_cmp_built_in.c
@@ -6,7 +6,7 @@
 /*   By: grezette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 15:01:30 by grezette          #+#    #+#             */
-/*   Updated: 2020/10/19 15:06:20 by grezette         ###   ########.fr       */
+/*   Updated: 2020/10/19 15:11:22 by grezette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ int		cmp_built_in(char **argv, t_cmd *cmd, t_cmdv *cmdv)
 		ft_cd(*cmd);
 	else if (ft_strncmp(argv[0], "pwd", 3))
 		ft_pwd(*cmd);
-	else if (ft_strncmp(argv[0], "env", 2))
+	else if (ft_strncmp(argv[0], "env", 3))
 		ft_env(*cmd, cmdv->envp);
-	else if (ft_strncmp(argv[0], "unset", 2))
+	else if (ft_strncmp(argv[0], "unset", 5))
 		ft_putstr_fd("A work in progress\n", 1);
-	else if (ft_strncmp(argv[0], "export", 2))
+	else if (ft_strncmp(argv[0], "export", 6))
 		ft_putstr_fd("A work in progress\n", 1);
-	else if (ft_strncmp(argv[0], "exit", 2))
+	else if (ft_strncmp(argv[0], "exit", 4))
 		ft_putstr_fd("A work in progress\n", 1);
 	else
 		exe = 0;
