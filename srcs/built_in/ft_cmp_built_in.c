4506b35b5c5fc9e0e630bcccc2edb39671a8203d
pierrecariou <pierrecariou@outlook.fr>
2020-10-20 11:38:17 +0200

    one line issue

--- srcs/built_in/ft_cmp_built_in.c
+++ srcs/built_in/ft_cmp_built_in.c
@@ -6,7 +6,7 @@
 /*   By: grezette <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/19 15:01:30 by grezette          #+#    #+#             */
-/*   Updated: 2020/10/20 11:26:19 by pcariou          ###   ########.fr       */
+/*   Updated: 2020/10/20 11:34:30 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int		cmp_built_in(char **argv, t_cmd *cmd, t_cmdv *cmdv)
 	if (!ft_strcmp(argv[0], "echo"))
 		ft_echo(*cmd);
 	else if (!ft_strcmp(argv[0], "cd"))
-		ft_cd(*cmd);
+		ft_cd(*cmd, cmdv);
 	else if (!ft_strcmp(argv[0], "pwd"))
 		ft_pwd(*cmd);
 	else if (!ft_strcmp(argv[0], "env"))
