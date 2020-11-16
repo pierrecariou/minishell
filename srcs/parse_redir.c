d4236008b01b34be560331f0dc998ba779897dd4
pierrecariou <pierrecariou@outlook.fr>
2020-11-16 18:08:44 +0100

    norme

--- srcs/parse_redir.c
+++ srcs/parse_redir.c
@@ -6,31 +6,16 @@
 /*   By: pcariou <pcariou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/15 11:34:16 by pcariou           #+#    #+#             */
-/*   Updated: 2020/11/16 10:13:15 by pcariou          ###   ########.fr       */
+/*   Updated: 2020/11/16 18:07:05 by pcariou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-int		get_redirfb(t_cmd *cmd, t_cmdv *cmdv, int i, int c)
+int		get_redirfb1(t_cmd *cmd, int c, int cp, int l)
 {
-	int cp;
-	int l;
-	int inquotes;
+	int i;
 
-	l = 0;
-	inquotes = cmdv->inquotess + cmdv->inquotesd;
-	while (ft_isspace(cmd->line[i]))
-		i++;
-	cp = i;
-	while (cmd->line[i] && !ft_isspace(cmd->line[i]) && (cmd->line[i] != '>' || inquotes))
-	{
-		inquotes = is_inquotes(cmd->line[i], cmdv);
-		l++;
-		i++;
-	}
-	if (!(cmd->redirfb[c] = malloc(sizeof(char) * (l + 1))))
-		return (0);
 	i = 0;
 	while (i < l)
 	{
@@ -43,17 +28,49 @@ int		get_redirfb(t_cmd *cmd, t_cmdv *cmdv, int i, int c)
 	return (cp);
 }
 
+int		get_redirfb(t_cmd *cmd, t_cmdv *cmdv, int i, int c)
+{
+	int cp;
+	int l;
+	int inquotes;
+
+	l = 0;
+	inquotes = cmdv->inquotess + cmdv->inquotesd;
+	while (ft_isspace(cmd->line[i]))
+		i++;
+	cp = i;
+	while (cmd->line[i] && !ft_isspace(cmd->line[i]) &&
+			(cmd->line[i] != '>' || inquotes))
+	{
+		inquotes = is_inquotes(cmd->line[i], cmdv);
+		l++;
+		i++;
+	}
+	if (!(cmd->redirfb[c] = malloc(sizeof(char) * (l + 1))))
+		return (0);
+	cp = get_redirfb1(cmd, c, cp, l);
+	return (cp);
+}
+
+int		get_redirb1(t_cmd *cmd, t_cmdv *cmdv, int i, int c)
+{
+	cmd->line[i++] = ' ';
+	cmdv->inquotes = is_inquotes(cmd->line[i], cmdv);
+	if (cmd->line[i] == '>' && !cmdv->inquotes)
+		cmd->line[i++] = ' ';
+	cmdv->inquotes = is_inquotes(cmd->line[i], cmdv);
+	i = get_redirfb(cmd, cmdv, i, c) - 1;
+	return (i);
+}
+
 void	get_redirb(t_cmd *cmd, t_cmdv *cmdv)
 {
 	int i;
 	int c;
-	int inquotes;
 
 	while (cmd)
 	{
-		inquotes = 0;
-		cmdv->inquotesd = 0;
-		cmdv->inquotess = 0;
+		init_inquotes(cmdv);
 		i = -1;
 		c = 0;
 		if (cmd->nredir > 1)
@@ -63,18 +80,9 @@ void	get_redirb(t_cmd *cmd, t_cmdv *cmdv)
 			cmd->redirfb[cmd->nredir - 1] = 0;
 			while (cmd->line[++i] && c < (cmd->nredir - 1))
 			{
-				inquotes = is_inquotes(cmd->line[i], cmdv);
-				if (cmd->line[i] == '>' && !inquotes)
-				{
-					cmd->line[i] = ' ';
-					i++;	
-					inquotes = is_inquotes(cmd->line[i], cmdv);
-					if (cmd->line[i] == '>' && !inquotes)
-						cmd->line[i++] = ' ';
-					inquotes = is_inquotes(cmd->line[i], cmdv);
-					i = get_redirfb(cmd, cmdv, i, c) - 1;
-					c++;
-				}
+				cmdv->inquotes = is_inquotes(cmd->line[i], cmdv);
+				if (cmd->line[i] == '>' && !cmdv->inquotes)
+					get_redirb1(cmd, cmdv, i, c++);
 			}
 		}
 		cmd = cmd->next;
@@ -85,7 +93,7 @@ void	count_redir(t_cmd *cmd, t_cmdv *cmdv)
 {
 	int i;
 	int inquotes;
-	
+
 	while (cmd)
 	{
 		inquotes = 0;
@@ -135,7 +143,6 @@ void	get_redirfl(t_cmd *cmd, int i)
 	cmd->redirfl[l] = 0;
 }
 
-
 void	get_redirf(t_cmd *cmd, int i)
 {
 	int cp;
@@ -163,39 +170,39 @@ void	get_redirf(t_cmd *cmd, int i)
 	cmd->redirf[l] = 0;
 }
 
+void	get_redir1(t_cmd *cmd, int i)
+{
+	if (cmd->line[i] == '<')
+		cmd->redirl = cmd->line[i];
+	else
+		cmd->redir = cmd->line[i];
+	if (cmd->line[i + 1] && cmd->line[i + 1] == '>')
+	{
+		cmd->redir = '}';
+		cmd->line[i + 1] = ' ';
+	}
+	if (cmd->line[i] == '>')
+		get_redirf(cmd, i + 1);
+	else if (cmd->line[i] == '<')
+		get_redirfl(cmd, i + 1);
+	cmd->line[i] = ' ';
+}
+
 void	get_redir(t_cmd *cmd, t_cmdv *cmdv)
 {
 	int i;
-	int inquotes;
 
 	while (cmd)
 	{
-		inquotes = 0;
-		cmdv->inquotess = 0;
-		cmdv->inquotesd = 0;
+		init_inquotes(cmdv);
 		i = -1;
 		cmd->redir = 0;
 		cmd->redirl = 0;
 		while (cmd->line[++i])
 		{
-			inquotes = is_inquotes(cmd->line[i], cmdv);
-			if ((cmd->line[i] == '<' || cmd->line[i] == '>') && !inquotes)
-			{
-				if (cmd->line[i] == '<')
-					cmd->redirl = cmd->line[i];
-				else
-					cmd->redir = cmd->line[i];
-				if (cmd->line[i + 1] && cmd->line[i + 1] == '>')
-				{
-					cmd->redir = '}';
-					cmd->line[i + 1] = ' ';
-				}
-				if (cmd->line[i] == '>')
-					get_redirf(cmd, i + 1);
-				else if (cmd->line[i] == '<')
-					get_redirfl(cmd, i + 1);
-				cmd->line[i] = ' ';
-			}
+			cmdv->inquotes = is_inquotes(cmd->line[i], cmdv);
+			if ((cmd->line[i] == '<' || cmd->line[i] == '>') && !cmdv->inquotes)
+				get_redir1(cmd, i);
 		}
 		cmd = cmd->next;
 	}
