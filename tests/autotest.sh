e04ce7c9b4dfa569639ae472dcdfb1f2a61da5e6
pierrecariou <pierrecariou@outlook.fr>
2020-11-19 10:22:05 +0100

    tester update

--- tests/autotest.sh
+++ tests/autotest.sh
@@ -1,6 +1,6 @@
 cd ..
 make
-./minishell < tests/test.sh > tests/compare.txt
+bash < ./minishell < tests/test.sh > tests/compare.txt
 bash < tests/test.sh > tests/compare1.txt 
 diff -y -W 120 --suppress-common-lines --color tests/compare.txt tests/compare1.txt
-rm tests/compare1.txt tests/compare.txt
+rm tests/compare.txt tests/compare1.txt
